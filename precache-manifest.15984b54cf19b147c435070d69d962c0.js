96d919e942a98218ddaf79f8bd086be51f6f36b5
Ivyzhao <1490217104@qq.com>
2022-03-22 21:42:26 +0800

    deploy

---
+++ precache-manifest.15984b54cf19b147c435070d69d962c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc2aae1e0efb390279a2",
+    "url": "/jizhang-1-website/css/app.f3c2035e.css"
+  },
+  {
+    "revision": "a17f99bf0b9a091b58c071f198353a2a",
+    "url": "/jizhang-1-website/index.html"
+  },
+  {
+    "revision": "bc2aae1e0efb390279a2",
+    "url": "/jizhang-1-website/js/app.86f3e7f6.js"
+  },
+  {
+    "revision": "54d7987b9d78fbac3c18",
+    "url": "/jizhang-1-website/js/chunk-vendors.a4d22c3c.js"
+  },
+  {
+    "revision": "ff270a0a0f67cbb5ab608fa0ac3a2f5e",
+    "url": "/jizhang-1-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/jizhang-1-website/robots.txt"
+  }
+]);
